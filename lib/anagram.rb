c9434ba011ee3ca66133a728968d01eaebd28115
Corey H <c.l.halicke@gmail.com>
2019-06-27 01:38:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(array)
     array.each do |word|
       if self.word.split("").sort = word.split("").sort
-        matches << word
+        matches << array[counter]
       end
       counter += 1
     end
