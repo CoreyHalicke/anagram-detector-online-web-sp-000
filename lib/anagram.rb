8ccab6027549a599e371e42c834dfa8069775673
Corey H <c.l.halicke@gmail.com>
2019-06-27 01:44:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,12 @@ class Anagram
     @word = word
   end
 
-  counter = 0
   matches = []
   def match(array)
     array.each do |word|
       if self.word.split("").sort == word.split("").sort
         matches << word
       end
-      counter += 1
     end
     matches
   end
